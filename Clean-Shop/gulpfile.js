c1197577c49e9511495225cad870295252ebd12a
Onivaldo Alcides Lotti Junior <39131958+onivaldolotti@users.noreply.github.com>
2020-07-14 11:07:37 -0300

    Add files via upload

---
+++ Clean-Shop/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./css/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'));
+});
+ 
+gulp.task('css', function () {
+  gulp.src('./css/*.css')
+    .pipe(uglifycss({
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
